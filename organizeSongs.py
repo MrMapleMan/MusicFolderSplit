12ab2fbf323905bf657dc8960b26a46440d68715
MrMapleMan <gimmeyerjunkmail@gmail.com>
2020-10-20 10:08:43 -0700

    Update organizeSongs.py

--- organizeSongs.py
+++ organizeSongs.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import eyed3
 
 numArgs = len(sys.argv)
 if numArgs > 0:
@@ -8,9 +9,23 @@ else:
   print('Please provide directory as first argument.')
   sys.exit()
 
-# Walk through all files (not folders)
+dirPath, dirNames, fileNames = os.walk(mainDir)
 
+# Walk through all files (not folders)
+for i in fileNames:
 # For every song:
-  # Check metadata to find artist
+  
+  fullPath = os.path.join(mainDir, i)
+  
+  # Check metadata to find artist  
+  audiofile = eyed3.load(fullPath)
+  newPath = os.path.join(mainDir, audiofile.tag.artist)
+  
   # Create artist folder if needed
+  try:
+    os.mkdir(newPath)
+  except:
+    pass
+  
   # Move song into folder
+  os.rename(fullPath, os.path.join(newPath, i))
